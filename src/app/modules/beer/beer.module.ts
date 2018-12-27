647766ce94a6a2c8fa94cd94b7966bf8019e52f3
Paweł Wollny <pawel@localhost.localdomain>
2018-12-27 11:54:09 +0100

    Add FormsModule to beer.module

--- src/app/modules/beer/beer.module.ts
+++ src/app/modules/beer/beer.module.ts
@@ -6,12 +6,14 @@ import { NgxImageGalleryModule } from 'ngx-image-gallery';
 import { BeerRoutingModule } from './beer-routing.module';
 import { BeerTableComponent } from './components/beer-table/beer-table.component';
 import { BeerTableColumnComponent } from './components/beer-table-column/beer-table-column.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BeerTableComponent, BeerTableColumnComponent],
   imports: [
     CommonModule,
     BeerRoutingModule,
+    FormsModule,
     MatButtonModule,
     MatIconModule,
     MatTableModule,
